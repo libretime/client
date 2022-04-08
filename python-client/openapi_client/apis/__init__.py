9137a4beee201d82d93538038a651f9e5fb89384
paddatrapper <paddatrapper@users.noreply.github.com>
2022-04-08 08:32:08 +0000

    Update clients

--- python-client/openapi_client/apis/__init__.py
+++ python-client/openapi_client/apis/__init__.py
@@ -6,7 +6,7 @@
 # raise a `RecursionError`.
 # In order to avoid this, import only the API that you directly need like:
 #
-#   from .api.celery_tasks_api import CeleryTasksApi
+#   from openapi_client.api.celery_tasks_api import CeleryTasksApi
 #
 # or import this package, but before doing it, use:
 #
